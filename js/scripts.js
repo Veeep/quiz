39839b90c4ea994c9a385ab8ef4515d865f7cbe8
Chris VP <cvpacupuncture@gmail.com>
2020-07-15 21:25:59 -0700

    add 5 input conversions to const inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    const timeInput = $("input#time").val();
+    const empireInput = $("input#empire").val();
+    const throneInput= $("input#throne").val();
+    const swordInput = $("input#sword").val();
+    const scaleInput = parseInt($("#scale").val());
+    
 
 
     event.preventDefault();
